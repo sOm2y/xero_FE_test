132e5ae9c60cf80df45976f0a6fc774e70907dc0
som2y <samyin1990@gmail.com>
2020-01-10 14:38:12 +1300

    update test

--- invoice.spec.js
+++ invoice.spec.js
@@ -3,19 +3,18 @@ const InvoiceLine = require('./invoiceLine.js')
 let invoiceMock
 
 beforeEach(() => {
-	// Clear all instances and calls to constructor and all methods:
 	invoiceMock = new Invoice(new Date(), 'N001')
 })
 
 describe('Invoice class', () => {
 	it('should call constructor', () => {
 		expect(invoiceMock.InvoiceNumber).toBe('N001')
-		expect(invoiceMock.LineItems.length).toBe(0)
+		expect(invoiceMock.LineItems).toHaveLength(0)
 	})
 
 	it('should add invoice line ', () => {
 		invoiceMock.AddInvoiceLine(new InvoiceLine(1, 0.99, 5, 'Onion'))
-		expect(invoiceMock.LineItems.length).toBe(1)
+		expect(invoiceMock.LineItems).toHaveLength(1)
 		expect(invoiceMock.LineItems[0].InvoiceLineId).toBe(1)
 	})
 
@@ -24,16 +23,29 @@ describe('Invoice class', () => {
 		invoiceMock.AddInvoiceLine(new InvoiceLine(2, 10.21, 4, 'Banana'))
 		invoiceMock.AddInvoiceLine(new InvoiceLine(3, 6.21, 5, 'Pineapple'))
 		invoiceMock.RemoveInvoiceLine(1)
-		expect(invoiceMock.LineItems.length).toBe(2)
+		expect(invoiceMock.LineItems).toHaveLength(2)
+		expect(invoiceMock.LineItems[0]).not.toHaveProperty('Description', 'Onion')
 	})
 
-	it('should merge invoices ', () => {})
+	it('should merge invoices ', () => {
+		invoiceMock.AddInvoiceLine(new InvoiceLine(1, 10.21, 1, 'Blueberries'))
+		const invoiceMock2 = new Invoice()
+		invoiceMock2.AddInvoiceLine(new InvoiceLine(2, 5.29, 4, 'Orange'))
+		invoiceMock.MergeInvoices(invoiceMock2)
+		expect(invoiceMock.LineItems).toHaveLength(2)
+		expect(invoiceMock.LineItems[1]).toHaveProperty('Description', 'Orange')
+	})
 
-	it('should get total cost', () => {})
+	it('should get total cost', () => {
+		invoiceMock.AddInvoiceLine(new InvoiceLine(1, 1, 5, 'Onion'))
+		invoiceMock.AddInvoiceLine(new InvoiceLine(2, 10, 4, 'Banana'))
+		invoiceMock.AddInvoiceLine(new InvoiceLine(3, 10, 5, 'Pineapple'))
+		expect(invoiceMock.GetTotal()).toBe('95.00')
+	})
 
 	it('should clone invoice ', () => {
 		const clonedInvoiceMock = invoiceMock.Clone()
-		expect(clonedInvoiceMock instanceof Invoice).toEqual(true)
+		expect(clonedInvoiceMock).toBeInstanceOf(Invoice)
 		expect(clonedInvoiceMock).toEqual(invoiceMock)
 	})
 })
